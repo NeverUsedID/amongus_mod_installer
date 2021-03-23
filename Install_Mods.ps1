1247f2d742e63edeb89ca1f23c38e938e9b5a4e5
NeverUsedID <Github@rafaelvoss.de>
2021-03-23 12:16:43 +0100

    Update and rename Install_ExtraRoles.ps1 to Install_Mods.ps1

---
+++ Install_Mods.ps1
@@ -0,0 +1,197 @@
+<#
+V0.3 - NeverUsedID
+Installiert ausgewählete Mods und bietet die Moeglichkeit an, Crewlink zu installieren, wenn keinen Crewlinkconfig gefunden wird. Setzt den Crewlinkserver auf Standard zurueck.
+
+HELP:
+
+#Ausfuehren, wenn scripte verboten sind:
+
+Moeglichkeit 1:
+Eingabeaufforderung oeffnen und folgenden Befehl ausfuehren (Pfad zur heruntergeladenenen Datei anpassen) 
+powershell -ExecutionPolicy ByPass -File C:\Users\username\Downloads\Install_ExtraRoles.ps1
+
+Moeglichkeit 2:
+Powershell starten und set-executionpolicy remotesigned ausführen. In Zukunft kann das Script dann direket ausgeführt werden.
+#>
+
+Param (
+[String]$releaseid = "",
+[String]$modid = ""
+)
+
+
+#
+# Configuration (change this urls to update Version)
+
+#Streamer wird für den Orndernamen verwendet, damit nicht versehentlich andere Verionen überschrieben werden
+$streamer = "NeverUsedID" #Keine Leerzeichen, keine Sonderzeichen!
+
+#
+# Wenn hier der Pfad zu Among us eingetragen wird, entfaellt die Suche nach Among Us, was den installer beschleunigt, vro allem auf nicht SSD Systemen
+$amonguspath=""
+
+
+#Crewlink URL
+$crewlinkurl="https://github.com/OhMyGuus/BetterCrewLink/releases/download/v2.4.0/Better-CrewLink-Setup-2.4.0.exe"
+$crewlinkserver = "https://bettercrewl.ink"
+
+
+#
+# Some configs
+#
+$defaultDisk="C:"
+
+#List your mods here with relative Github Download paths:
+#Hier koennen die Mods mit relativen Githubpfaden angegeben werden, die installiert werden koennen.
+$modurls = @{}
+$modurls.add("TownOfUs", "/slushiegoose/Town-Of-Us/releases")
+$modurls.add("ExtraRoles", "/NotHunter101/ExtraRolesAmongUs/releases")
+
+
+
+########
+# Main #
+########
+
+#set default values 
+$AmongUsDisk=""
+$steamappsfolders =""
+
+#Ask for mod number to install it later
+$modnumber = 0
+ForEach ( $mod in $modurls.Keys ) {
+  write-host "$modnumber - $mod"
+  $modnumber++
+}
+if ( $modId -eq "" ) {
+  $modId = read-host "Welchen Mod willst du installieren (Nummer angeben)?"
+}
+$mod = $modurls.keys | select -index $modId
+
+#URLs bauen
+$githubUrl = "https://github.com"
+$modSubUrl = $modurls.Values | select -index $modId
+$modurl = "$githubUrl$modSubUrl"
+
+$folderExtension = "$($mod) - $($streamer)"
+
+#Get Releases from Github latest
+$latestReleaseContent = Invoke-WebRequest "$modUrl/latest" | Select-String "releases/download"
+$latestReleaseContent = $latestReleaseContent.ToString()
+$releases = @()
+#regex to get Zip Download URL
+$modurlregex = ".*" + "$($modSubUrl.Replace("/", "\/"))" + "(.*.zip).*"
+
+#Search for Zipfiles on release page
+foreach ( $line in $latestReleaseContent.split("`n")) {
+  if ( $($line |  Select-String "releases/download" | Select-String ".zip") -match ".zip" ) {
+    $releases += ($line |  Select-String "releases/download" | Select-String ".zip") -replace "$modurlregex", '$1'
+  }
+}
+
+#Ask which releas should be downloaded
+$releasenumber = 0
+ForEach ( $release in $releases ) {
+  write-host "$releasenumber - $release"
+  $releasenumber++
+}
+if ( $releaseId -eq "" ) {
+  $releaseId = read-host "Welches Release soll genutzt werden, meißt is es `"0`". Bei `"Town of us`" muss auf das neuere Monatsdatum geachtet werden!"
+}
+$modDownloadUrl = "$modurl$($releases[$releaseId])"
+
+# Search Among Us folder on local system
+$installationFound = $false
+if ( $amonguspath -eq "" ) {
+#check default Steam folder
+  if ( test-path "C:\Program Files (x86)\Steam\steamapps\common\Among Us" ) {
+    $amonguspath = "C:\Program Files (x86)\Steam\steamapps\common\Among Us"
+    $installationFound = $true
+    Write-host "Among us gefunden:"
+    Write-Host "$amonguspath" -ForegroundColor Magenta
+  } else {
+    #search Amonug us on selected disk
+    write-host "INFO: Among Us nicht gefunden - Es muss automatisch gesucht werden!" -ForegroundColor Yellow
+    write-host "INFO: Among Us Pfad kann im Script angegeben werden, dann wird die installation stark beschleunigt." -ForegroundColor Cyan
+    if (!($AmongUsDisk = Read-Host "Bitte das Laufwerk auf dem Steam bzw. die Bibliothek installiert ist angeben (z.B. `"D:`") - Enter druecken fuer`"$defaultDisk`"")) { $AmongUsDisk= $defaultDisk }
+    $steamappsfolders = Get-ChildItem $AmongUsDisk\ -recurse -ErrorAction SilentlyContinue | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "steamapps"} 
+    ForEach ( $steamappsfolder in $steamappsfolders ) {
+      if ( test-path "$($steamappsfolder.FullName)\common\Among Us"  ) {
+        $amonguspath = "$($steamappsfolder.FullName)\common\Among Us"
+        $installationFound = $true
+        write-host "Among Us gefunden:"
+        Write-Host "$amonguspath" -ForegroundColor Magenta
+      }
+    }
+  }
+}
+
+#
+# ConfigureBetter Crewlink
+
+function Update-CrewlinkServer {
+  if ( $crewlinkconfig.serverURL -ne "$crewlinkserver" ) {
+    $updateCrewlinkServer = read-host "Der Bettercrewlink server ist nicht `"$crewlinkserver`" Soll er angepasst werden? (j/n)" 
+    if ( $updateCrewlinkServer -eq "j" ) {
+      $crewlinkconfig.serverURL = $crewlinkserver
+      copy-item "$env:APPDATA\bettercrewlink\config.json" "$env:APPDATA\bettercrewlink\config.json.bak" -force
+      # $crewlinkconfig | ConvertTo-Json - | out-file $env:APPDATA\bettercrewlink\config.json
+      $file = "$env:APPDATA\bettercrewlink\config.json"
+      $regex = '^(.*"serverURL": ).*$'
+      $replace ="`t`"serverURL`": `"$crewlinkserver`","
+      (Get-Content $env:APPDATA\bettercrewlink\config.json) -replace "$regex", "$replace" | Set-Content $file
+
+    }
+  }
+}
+
+#Check if a BetterCrewlink Config exists and ask for installation of Crewlink, if not.
+if ( test-path "$env:APPDATA\bettercrewlink" ) { 
+  $crewlinkconfig = get-content "$env:APPDATA\bettercrewlink\config.json" | convertfrom-json
+  Update-CrewlinkServer
+} else {
+  $installbettercrewlink = read-host "Better Crewlink nicht gefunden, soll es heruntergeladen und installiert werden? (j/n)"
+  if ( $installbettercrewlink -eq "j" ) {
+    #
+    # Download Better Crewlink
+    Write-host "Better Crewlink wird heruntergeladen" -ForegroundColor Cyan
+    Invoke-WebRequest -Uri $crewlinkurl -OutFile BetterCrewLinkSetup.exe
+    Write-host "Setup wird ausgefuehrt, einfach durchklicken" -ForegroundColor Cyan
+    Start-Process BetterCrewLinkSetup.exe -wait
+  }
+}
+
+
+#Install Among Us Mod, if Among us is found
+if ( $installationFound ) {
+  #
+  # Download mod
+   
+  Write-host "Mod wird heruntergeladen" -ForegroundColor Cyan
+  Invoke-WebRequest -Uri $modDownloadurl -OutFile "$mod.zip"
+  Write-host "Mod wird entpackt" -ForegroundColor Cyan
+  Expand-Archive -LiteralPath "$mod.zip" -DestinationPath "$mod" -force
+  Write-host "Mod wird in $amonguspath - $folderExtension integriert" -ForegroundColor Cyan
+  Copy-Item -path "$amonguspath" -Destination "$amonguspath - $folderExtension" -Recurse -Force -Container 
+  Copy-Item -path "$mod\*" -Destination "$amonguspath - $folderExtension\" -Recurse -Force 
+  Write-host "Verknuepfung `"Among Us - $mod - $streamer.lnk`" wird auf Desktop erstellt" -ForegroundColor Cyan
+  $linkPath        = Join-Path ([Environment]::GetFolderPath("Desktop")) "Among Us - $mod.lnk"
+  $targetPath      = Join-Path "$amonguspath - $folderExtension" "Among Us.exe"
+  $link            = (New-Object -ComObject WScript.Shell).CreateShortcut($linkPath)
+  $link.TargetPath = $targetPath
+  $link.Save()
+  $startAmongUs = read-host "Among Us direkt starten? (j/n)"
+  if ( $startAmongUs -eq "j" ) {
+    . "$amonguspath - $folderExtension\Among Us.exe"
+  } else { 
+    read-host "Enter zum schliessen"
+  }
+} else {
+  Write-host "Fehler: Among Us nicht gefunden!" -ForegroundColor Yellow
+  read-host "Enter zum schliessen"
+}
+
+
+
+
+
