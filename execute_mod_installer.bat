355808b5ca101e7a3902f6b3a86aaae66a860999
NeverUsedID <Github@rafaelvoss.de>
2021-03-25 11:03:46 +0100

    Update execute_mod_installer.bat

--- execute_mod_installer.bat
+++ execute_mod_installer.bat
@@ -2,7 +2,8 @@
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath=%ThisScriptsDirectory%Install_Mods.ps1
 
-rem you can preset modid and releaseid here to autoinstall required Version / "rem" is to skip lines in execution
-rem PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'" -args -args -releaseid 0 -modid 0;
+#you can preset modid and releaseid here to autoinstall required Version.
+#PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'" -args -streamer NeverUsedID -releaseid 0 -modid 0;
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'" -args -streamer NeverUsedID;
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'"
+#PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'"
